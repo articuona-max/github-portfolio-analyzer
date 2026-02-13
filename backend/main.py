f7fdac53a6861ebe5fb3b024e09c6b4546c279f2
articuona-max <aravind22105@gmail.com>
2026-02-13 23:36:31 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ def create_application() -> FastAPI:
     from fastapi.middleware.cors import CORSMiddleware
     application.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
